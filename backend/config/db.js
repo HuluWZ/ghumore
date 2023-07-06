b69aa4f4d843d24ae22e892f7bdbc5b40263c20a
Hulunlante Worku <hulunlante.w@gmail.com>
2023-07-06 14:39:14 +0300

    minor check

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const URL_LOCAL = process.env.REMOTE_MONGODB_URL
-
+console.log(" URL_LOCAL ", URL_LOCAL);
 mongoose.set("strictQuery", true);
 mongoose.connect(URL_LOCAL).then(() => console.log('Connected!'));
 
