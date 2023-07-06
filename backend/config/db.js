5af533b592429e78916340d4839caf9e8efb8f58
Hulunlante Worku <hulunlante.w@gmail.com>
2023-07-06 14:46:36 +0300

    test deploy

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,7 @@
-const mongoose = require("mongoose");
+const {set,connect} = require("mongoose");
 require("dotenv").config();
 const URL_LOCAL = process.env.REMOTE_MONGODB_URL
-console.log(" URL_LOCAL ", URL_LOCAL);
-mongoose.set("strictQuery", true);
-mongoose.connect(URL_LOCAL).then(() => console.log('Connected!'));
-
-mongoose.set("runValidators", true);
+// console.log(" URL_LOCAL ", URL_LOCAL);
+set("strictQuery", true);
+connect(URL_LOCAL).then(() => console.log('Connected!'));
+set("runValidators", true);
