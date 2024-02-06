e62414385843fa240e0127f7f84c0b454ce9cd3c
HuluWZ <hulunlante.w@gmail.com>
2024-02-06 20:14:45 +0300

    Add proxy and update baseURL in package.json and axiosinstance.js

--- frontend/src/apiCalls/axiosinstance.js
+++ frontend/src/apiCalls/axiosinstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:4000/",
-  baseURL: "https://ghumore-travel.onrender.com/",
+  baseURL: "https://ghumore-travel.onrender.com/", 
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
